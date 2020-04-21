5e03e31036b9f4009d5016a605f1623cb8cde9cc
billy.santos <billy.santos@whitecloak.com>
2020-04-21 13:53:28 +0800

    Added next config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	exportPathMap: async function (
+		defaultPathMap,
+		{ dev, dir, outDir, distDir, buildId }
+	) {
+		return {
+			"/": { page: "/" },
+			"/about": { page: "/about" },
+			"/contact-me": { page: "/contact-me" },
+			skills: { page: "/skills" },
+		};
+	},
+};
